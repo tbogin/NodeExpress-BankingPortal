2b77287aaecf9e8f6ed7adf202c77c993b0dfe1b
ted.bogin <ted.bogin@accenture.com>
2019-02-15 11:42:42 -0500

    Fixed spelling errors

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const path = require('path');
 const express = require('express');
 const { accounts, users, writeJSON } = require('./data');
 const accountRoutes = require('./routes/accounts');
-const serviceRoutes = require('./routes/services');
+const servicesRoutes = require('./routes/services');
 
 const app = express();
 
@@ -19,7 +19,7 @@ app.get('/', (req, res) => {
 });
 
 app.use('/account', accountRoutes); //wire up account and services routers
-app.use('/services', serviceRoutes);
+app.use('/services', servicesRoutes);
 
 app.get('/profile', (req, res) => {
   res.render('profile', {user: users[0]})
