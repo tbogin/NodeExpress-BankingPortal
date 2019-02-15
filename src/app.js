1bb2db30a03e04de1c66737beb8c790910850600
ted.bogin <ted.bogin@accenture.com>
2019-02-15 11:26:41 -0500

    Completred Creating a Data Access Library

--- src/app.js
+++ src/app.js
@@ -4,20 +4,13 @@ const express = require('express');
 
 const app = express();
 
+const { accounts, users, writeJSON } = require('./data');
+
 app.set('views', path.join(__dirname, 'views')); //configure views/
 app.set('view engine', 'ejs'); //set config property for view engine to ejs
 
 app.use(express.static(path.join(__dirname, 'public'))); //find static directory, and serve those files
 app.use(express.urlencoded({extended: true}));
-
-const accountData = fs.readFileSync(
-  path.join(__dirname, 'json', 'accounts.json'), 'utf8'
-);
-const accounts = JSON.parse(accountData);
-const userData = fs.readFileSync(
-  path.join(__dirname, 'json', 'users.json'), 'utf8'
-);
-const users = JSON.parse(userData);
 
 app.get('/', (req, res) => {
   res.render('index', {title: 'Account Summary', accounts}); //render index view with associated title
@@ -38,8 +31,7 @@ app.get('/transfer', (req, res) => {
 app.post('/transfer', (req, res) => {
   accounts[req.body.from].balance = accounts[req.body.from].balance - req.body.amount;
   accounts[req.body.to].balance = parseInt(accounts[req.body.to].balance) + parseInt(req.body.amount, 10);
-  const accountsJSON = JSON.stringify(accounts, null, 4);
-  fs.writeFileSync(path.join(__dirname, 'json/accounts.json'), accountsJSON, 'utf8');
+  writeJSON();
   res.render('transfer', { message: 'Transfer Completed' });
 });
 
@@ -49,8 +41,7 @@ app.get('/payment', (req, res) => {
 app.post('/payment', (req, res) => {
   accounts.credit.balance -= req.body.amount;
   accounts.credit.available += parseInt(req.body.amount, 10);
-  const accountsJSON = JSON.stringify(accounts, null, 4);
-  fs.writeFileSync(path.join(__dirname, 'json', 'accounts.json'), accountsJSON, 'utf8');
+  writeJSON();
   res.render('payment', {message: 'Payment successful', account: accounts.credit})
 });
 
